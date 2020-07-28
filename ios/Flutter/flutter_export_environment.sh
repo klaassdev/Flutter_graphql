a7c55e4c1a3c60a5645fe26531806eb02ebb04ee
michelo243 <misamuna@gmail.com>
2020-07-29 00:07:38 +0200

    -a
    part1 project init

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/michelo/Documents/Tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/michelo/Documents/Klaass/from_github/flutter_graphQL/flutter_graphql"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/michelo/Documents/Tools/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
